b8627e7dd1ebaa27c6c791aea00a6fea235b87e6
Luis Garcia <garcia@yahoo.com>
2017-12-21 16:10:09 -0700

    Complete functionality for I5

--- server.rb
+++ server.rb
@@ -1,21 +1,17 @@
 require 'socket'
-# require_relative 'supporting_paths'
 require 'date'
 require_relative 'word_search'
 require_relative 'game'
 
 class Server
   attr_reader :server, :supporting_paths 
-
   attr_accessor :shutdown_server
 
   def initialize
     @server              = TCPServer.new 9292
-    # @supporting_paths    = SupportingPaths.new
     @server_on           = true   
     @total_request_count = 0
     @hello_count         = 0
-    @response_code       = "http/1.1 200 OK"
     @game_count          = 0
   end
 
@@ -36,7 +32,6 @@ class Server
         post_path
       end
       
-      @response_code = "http/1.1 200 OK"
       puts "sent response"
     end
     @client.close
@@ -68,13 +63,11 @@ class Server
 
   def start_game
     if @game_count >= 1 
-      @response_code = "http/1.1 403 Forbidden"
-      response("There is already a game in progress.")
+      response("There is already a game in progress.", {status: "http/1.1 403 Forbidden"})
     else
       @game = Game.new
       @game_count += 1
-      @response_code = "http/1.1 301 Moved Permanently"
-      response("Good Luck!")
+      response("Good Luck!", {status: "http/1.1 301 Moved Permanently"})
     end
   end
 
@@ -83,13 +76,14 @@ class Server
   end
 
   def force_error
-    @response_code = "http/1.1 500 Internal Server Error"
-    response("SystemError")
+    response("SystemError", {status: "http/1.1 500 Internal Server Error"})
+  end
+
+  def not_found
+    response("Not Found", {status: "http/1.1 404 Not Found"})
   end
 
   def post_game(content_body)
-    # param = content_body[/guess[^-]*/].chomp if !content_body[/guess[^-]*/].nil?
-    # guess = param[/\d+/] if !param.nil?
     guess = content_body.split("\r\n")[3] if !content_body.nil?
     @game.number(guess)
     headers = ["HTTP/1.1 302 redirect", "location: /game",
@@ -120,53 +114,40 @@ class Server
         game
       when "/force_error"
         force_error
+      else
+        not_found  
     end
   end
 
   def post_path
     content_length = @request_lines[3].split(" ")[1].to_i
-    #137
     @content_body = @client.read(content_length)
-    #"------WebKitFormBoundarypkTbXIA9E8MdzgaM\r\nContent-Disposition: 
-    #       form-data; name=\"guess\"\r\n\r\n3\r\n------WebKitFormBoundarypkTbXIA9E8MdzgaM--\r\n"
     case path
       when "/start_game"
         start_game
       when "/game"
         post_game(@content_body)
+      else
+        not_found
     end
   end
 
   def response(body, headers = {})
     output = "<html><head></head><body> #{body} </body></html>"
-    # headers = ["http/1.1 200 ok",
-    #             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-    #             "server: ruby",
-    #             "content-type: text/html; charset=iso-8859-1",
-    #             "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    headers[:content_length] = output.length 
-    @client.puts res_headers(headers, @response_code)
+    status = headers[:status] || "http/1.1 200 ok"
+    headers = [status,
+                "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+                "server: ruby",
+                "content-type: text/html; charset=iso-8859-1",
+                "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    @client.puts headers
     @client.puts output
-  end
-
-  def res_headers(headers, response_code) #= "http/1.1 200 OK"
-    # respond_code = "http/1.1 200 OK" if response_code.nil?
-    status =  headers[:status] || response_code
-    [status,
-      "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-      "server: ruby",
-      "content-type: text/html; charset=iso-8859-1",
-      "content-length: #{headers[:content_length]}\r\n\r\n"].join("\r\n")
   end
 
   def path
     req_path = @request_lines[0].split(" ")[1]
-    if !req_path.include?("/", "hello", "datetime", "shutdown", "word_search",
-                         "game", "start_game", "force_error")
-      @response_code = "http/1.1 404 Not Found"
-      response("This is not a valid search!")
-    elsif req_path.include?("word_search")
-      @sample_word =  req_path.split("=")[1]
+    if req_path.include?("word_search")
+      @sample_word = req_path.split("=")[1]
       req_path = "/word_search"
     end
     req_path 
